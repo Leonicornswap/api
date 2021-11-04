bc354863aad1880ebfd0d8c9c1d8c500675134f1
Crypto0390 <crypto0390@gmail.com>
2021-11-04 04:50:47 -0700

    update temp endpoint

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -5,7 +5,7 @@ export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
     // uri: "https://api.bscgraph.org/subgraphs/name/pancakeswap/exchange-lite",
-    uri: "https://api.thegraph.com/subgraphs/name/crypto0390/leonicornexchange",
+    uri: "https://api.thegraph.com/subgraphs/name/crypto0390/leonicornswap",
   }),
   cache: new InMemoryCache(),
 });
